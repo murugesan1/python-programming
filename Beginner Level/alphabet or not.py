f77d3e224d517ff4cd216295b21799b84a00ed6b
tonykarthi <37359818+tonykarthi@users.noreply.github.com>
2018-03-19 23:25:36 +0530

    Create alphabet or not.py

---
+++ Beginner Level/alphabet or not.py
@@ -0,0 +1,5 @@
+x=str(input("enter the value"))
+    if x in ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
+        print("%s is Alphabet"%x)
+    else:
+        print("%s No"%x)
