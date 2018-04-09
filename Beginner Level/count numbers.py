5025e98ddd96af8bc54ac8eecfd6e6b8a499a652
tonykarthi <37359818+tonykarthi@users.noreply.github.com>
2018-04-09 12:41:59 +0000

    Create count numbers.py

---
+++ Beginner Level/count numbers.py
@@ -0,0 +1,11 @@
+a=int(input('enter the number'))
+count=0
+while(a>0):
+  count=count+1
+  a=a//10
+print('length of number:',count)n=int(input("Enter number:"))
+count=0
+while(n>0):
+    count=count+1
+    n=n//10
+print("The number of digits in the number are:",count)
